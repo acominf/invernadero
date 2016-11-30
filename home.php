d14b9fe49ae9b5ee96e9296cb256aa53d10da040
poqqen <poqqen31@gmail.com>
2016-11-30 15:27:21 -0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,89 @@
+
+<!DOCTYPE html>
+<html lang="es">
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+<head>
+	<meta charset="UTF-8">
+	<title>Invernadero</title>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand">Ferti-irrigacion</a>
+    </div>
+  </div>
+</nav>
+<div class="container col-md-12">
+<img src="invernadero.jpg" width="100%" height="500px;"><br><br><br>
+<div class="container col-md-12">
+<div class="container col-md-4" style="">
+	<img src="objetivo.jpg" class="img-circle center-block" width="80%;"><br><br>
+	<h1>Objetivo</h1>
+	<p style="text-align: justify;">
+		Es la automatizacion de un sistema de fertirrigación en base a las necesidades de las plantas, este sistema cuenta con monitorizacion de factores como humedad de la tierra, humedad relativa, temperatura, PH, CE, luminocidad.
+		El usuario puede monitorizar el sistema a travez de la pagina web donde puede conocer en tiempo real los valores de el cultivo en cuanto las variables climaticas. movimientos, riego y por quien se han realizado movimientos.
+	</p>
+</div>
+<div class="container col-md-4">
+	<img src="justificacion.png" class="img-circle center-block" width="70%;">	
+	<h1>Justificación</h1>
+	<p style="text-align: justify;">
+		En un invernadero automatizado se puede aumentar la cantidad, calidad y efectividad en la producción. Esto porque es a través de ellos que se pueden resolver problemas no sólo climáticos, sino de uso más eficiente de agua, nutrientes y controlar mejor plagas y enfermedades. Además de volver más eficiente el cultivo.
+		Si promovemos el uso de la automatización de los invernaderos el riesgo de intoxicaciones de los trabajadores por pesticidas seria considerablemente resuelto. También este proyecto busca beneficiar directamente tanto al sistema de cultivo tradicional (campo abierto) como al sistema de ambiente controlado.
+	</p>
+</div>
+<div class="container col-md-4">
+	<img src="antecedentes.png" class="img-circle center-block" width="80%;"><br>
+	<h1>Antecedentes</h1>
+	<p style="text-align: justify;"><br>
+	Fase 1:<br>
+	Desarollo del sistema de hardware, adquisiscion e implementacion de sensores <br>
+	Fase 2:<br>
+	Implementacion de la primera parte del desarollo de la pagina web<br>
+	Fase 3:<br>
+	Implementacion de sistema de riego 
+	<br>
+
+	</p><br><br><br>
+</div><br><br><br>
+<div class="container col-md-4" ><br>
+	<img src="metodologia.jpg" class="img-circle center-block" width="80%;">	<br><br>
+	<h1>Metodología</h1>
+	<p style="text-align: justify;">
+	Para obtener una metodologia ágil y flexible se utilizo el framework scrum de esta forma poder gestionar el desarrollo de software, cuyo principal objetivo es maximizar el retorno de la inversión para su empresa (ROI). Se basa en construir primero la funcionalidad de mayor valor para el cliente y en los principios de inspección continua, adaptación, auto-gestión e innovación.
+	Nos guiamos en esta metodologia el cual se divide en etapas de desarollo "Sprint" en el cual se trata de buscar las necesidades del cliente.
+
+		
+	</p>
+</div>
+<div class="container col-md-4">
+    <img src="resultados.jpg" class="img-circle center-block" width="80%;">	
+	<h1>Resultados</h1>
+	<p  style="text-align:  justify">
+	Con la implementacion del sistema el usuario tiene mas control y esta mas al tanto del sistema de riego,cultivo y movimientos realizados en el sistema
+	se puede obtener una mayor eficiencia de crecimiento de las plantas ya que los micronutrientes y macronutrientes estaran controlados para un mejor consumo.
+	</p>
+	
+</div>
+<div class="container col-md-4">
+ <img src="conclusiones.jpg" class="img-circle center-block" width="60%;">	
+	<h1>conlusiones</h1>	
+	<p style="text-align: justify;">
+	 Con la implementacion de este sistema se pueden monitorizar muchas de las variables climaticas, brindandole a las plantas el recurso necesario de fertilizante y nutrientes para su buen cultivo.
+     gracias a esto se podria remover de la ecuacion a un  encargado de invernadero, de esta forma el ingeniero solo se tiene que preocupar por la investigacion y los resultados.
+	</p>
+	
+</div>
+
+                  
+                   
+                  
+	</body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->    
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</html>
